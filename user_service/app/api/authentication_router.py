4c8ce54fe8c10aeb89036836566d6c22241d16d4
Em <emiliaburdak@gmail.com>
2024-05-26 10:18:03 +0200

    Extend jwt data

--- user_service/app/api/authentication_router.py
+++ user_service/app/api/authentication_router.py
@@ -38,5 +38,5 @@ def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depend
                             detail="Incorrect username or password",
                             headers={"WWW-Authenticate": "Bearer"},
                             )
-    access_token = create_access_token(data={"sub": user.username})
+    access_token = create_access_token(data={"sub": user.username, 'user_id': user.id, 'email': user.email})
     return {"access_token": access_token, "token_type": "bearer"}
