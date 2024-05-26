fbc93539bf8e2a9d9c1e38bc7daf3973f1795ba8
Em <emiliaburdak@gmail.com>
2024-05-26 23:20:16 +0200

    feat: Recreate cart when payment fail

--- booking_service/app/services/payment_service.py
+++ booking_service/app/services/payment_service.py
@@ -1,5 +1,6 @@
 import random
 
+from ..api.booking_router_commands import add_to_cart
 from ..database import get_db
 from ..models import PurchasedFlight, CartItem
 from ..services.kafka_events_utils import kafka_router
@@ -9,13 +10,27 @@ from ..services.kafka_events_utils import kafka_router
 @kafka_router.subscriber("payment-request")
 async def handle_payment_request(msg):
     print('Received message to handle payment: {}'.format(msg))
-
+    db = get_db()
     new_payment_status = simulate_payment_status()
-    await update_payment_status_in_db(msg.user_id, msg.purchase_ids, new_payment_status, db=get_db())
+    await update_payment_status_in_db(msg.user_id, msg.purchase_ids, new_payment_status, db)
+    if new_payment_status == "PaymentFailed":
+        await recreate_cart(db, msg)
 
 
-async def simulate_payment_status() -> bool:
-    return random.choices([True, False], weights=[0.8, 0.2])[0]
+async def recreate_cart(db, msg):
+    for purchase_id in msg.purchase_ids:
+        purchased_flight = db.query(PurchasedFlight).filter(
+            PurchasedFlight.user_id == msg.user_id,
+            PurchasedFlight.id == purchase_id
+        ).first()
+        flight_id = purchased_flight.flight_id
+        await add_to_cart(flight_id, msg.user_id, db=get_db())
+
+
+async def simulate_payment_status() -> str:
+    payment_response = random.choices([True, False], weights=[0.8, 0.2])[0]
+    new_payment_status = "PaymentSucceeded" if payment_response else "PaymentFailed"
+    return new_payment_status
 
 
 async def update_payment_status_in_db(user_id, purchase_ids, new_payment_status, db):
@@ -26,7 +41,6 @@ async def update_payment_status_in_db(user_id, purchase_ids, new_payment_status,
         ).first()
 
         purchased_flight.payment_status = new_payment_status
-        db.add(purchased_flight)
 
     db.commit()
 
