c69d893fe82e2a6f906af54bde9cd0151971435b
Em <emiliaburdak@gmail.com>
2024-05-26 10:18:40 +0200

    Retrieve user_id from jwt in booking service

--- booking_service/app/services/booking_service.py
+++ booking_service/app/services/booking_service.py
@@ -22,11 +22,10 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 SECRET_KEY = os.getenv("USER_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
 
-
 def get_current_user_id(token: str = Depends(oauth2_scheme)) -> int:
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        user_id: int = payload.get("sub")
+        user_id: int = payload.get("user_id")
         if user_id is None:
             raise HTTPException(status_code=403, detail="Could not validate credentials")
         return int(user_id)
