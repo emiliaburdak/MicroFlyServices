123eb6ea85b67ad3496dfd9a709f7ea0fc42c9e9
Em <emiliaburdak@gmail.com>
2024-05-30 18:20:34 +0200

    fix: Add leading slash to endpoint path in user_service to resolve proxy issue

--- user_service/app/api/authentication_router.py
+++ user_service/app/api/authentication_router.py
@@ -17,7 +17,7 @@ def get_db():
         db.close()
 
 
-@router.post("/register", response_model=schema.UserOutput)
+@router.post("/register/", response_model=schema.UserOutput)
 def register(user: schema.UserCreate, db: Session = Depends(get_db)):
     db_user = db.query(models.User).filter(models.User.email == user.email).first()
     if db_user:
@@ -30,7 +30,7 @@ def register(user: schema.UserCreate, db: Session = Depends(get_db)):
     return db_user
 
 
-@router.post("/login")
+@router.post("/login/")
 def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     user = authenticate_user(form_data.username, form_data.password, db)
     if not user:
